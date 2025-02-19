543a99b883c4763eb51cd520157be653a63ae502
Foysal Mohammad <foysal@earlblue.com>
2025-02-19 11:38:48 +0000

    feat: initial dev environment setup with webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"), // output directory
+    clean: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"], // resolve these file extensions
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // regex to match .ts and .tsx files
+        use: "babel-loader", // use babel-loader to transpile TypeScript
+        exclude: /node_modules/, // don't process node_modules
+      },
+      {
+        test: /\.js$/, // for regular JavaScript files (React components etc.)
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"), // serve static files
+    port: 3000, // your server port
+  },
+};
