c08f5a22bd0052c4dda7d90af2f9e23484fbf9ae
Foysal Mohammad <foysal@earlblue.com>
2025-02-20 10:47:38 +0000

    feat: implemented SSR with express

---
+++ server/server.tsx
@@ -0,0 +1,46 @@
+import express from "express";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import fs from "fs";
+import path from "path";
+
+import { App } from "../src/App/App";
+
+const PORT = 3000;
+const app = express();
+const queryClient = new QueryClient();
+
+// Serve static assets (like JS and CSS)
+app.use(express.static(path.resolve(__dirname, "../dist")));
+
+// Function to create the HTML string with SSR
+const createHtml = async () => {
+  const app = ReactDOMServer.renderToString(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
+
+  const html = await fs.promises.readFile(
+    path.resolve(__dirname, "../dist/index.html"),
+    "utf-8"
+  );
+
+  const reactHtml = html.replace(
+    '<div id="root"></div>',
+    `<div id="root">${app}</div>`
+  );
+  return reactHtml;
+};
+
+// Handle all routes by sending the SSR page
+app.get("*", async (req, res) => {
+  const html = await createHtml();
+  res.send(html);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App launched on ${PORT}`);
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { hydrateRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import "./index.css";
+
 import { App } from "./App/App";
 
 const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(
+hydrateRoot(
+  document.getElementById("root")!,
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
