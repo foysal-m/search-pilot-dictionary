244b2e6c7e59184ade44478b14ca0f236278c61f
Foysal Mohammad <foysal@earlblue.com>
2025-02-21 00:41:50 +0000

    fix: imported i18n to remove warning

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { App } from "./App/App";
+import "./i18n";
 
 const queryClient = new QueryClient();
 
